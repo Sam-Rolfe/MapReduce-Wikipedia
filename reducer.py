cc35c2f46c84dee63ca17b87547de3912a401c28
Ranvir <ranvir.malik@tufts.edu>
2024-12-11 18:19:00 -0500

    reducer code

--- reducer.py
+++ reducer.py
@@ -9,22 +9,26 @@ import sys
 # ----GLOBAL VARIABLES----------------------------------------------------------------------------
 prev_key = ""
 prev_count = 0
-item_count = 0
+first_item = True
 #----FUNCTIONS------------------------------------------------------------------------------------
 def reduce(name, view_count):
+    # Declare global variables
+    global prev_key, prev_count, first_item
+
     # First file being read in
     if prev_key == name:
         prev_count += view_count
     else:
-        if item_count != 0:
-            print(prev_key + '\t' + prev_count) 
+        if not first_item:
+            print(prev_key + '\t' + prev_count, end="") 
         prev_count = view_count
         prev_key = name
-        item_count = 1
+        first_item = False
 
 #----MAIN-----------------------------------------------------------------------------------------
 for line in sys.stdin:
+    line = line.lstrip()
     info_arr = line.split('\t')
     if (len(info_arr) != 2):
-        print(info_arr)
-    # reduce(info_arr[0], info_arr[1])
+        continue
+    reduce(info_arr[0], info_arr[1])
