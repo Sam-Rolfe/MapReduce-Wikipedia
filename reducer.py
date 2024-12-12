0d76064e725f23b64c4c4cd202dc22c5b3979184
Ranvir <ranvir.malik@tufts.edu>
2024-12-12 16:56:02 -0500

    reducer code

--- reducer.py
+++ reducer.py
@@ -14,21 +14,21 @@ first_item = True
 def reduce(name, view_count):
     # Declare global variables
     global prev_key, prev_count, first_item
-
     # First file being read in
     if prev_key == name:
-        prev_count += view_count
+        prev_count += int(view_count)
     else:
         if not first_item:
-            print(prev_key + '\t' + prev_count, end="") 
-        prev_count = view_count
+            print(prev_key + '\t' + str(prev_count)) 
+        prev_count = int(view_count)
         prev_key = name
         first_item = False
 
 #----MAIN-----------------------------------------------------------------------------------------
 for line in sys.stdin:
-    line = line.lstrip()
+    line = line.strip()
     info_arr = line.split('\t')
-    if (len(info_arr) != 2):
+    if (len(info_arr) != 2) or not info_arr[1].strip().isdigit(): 
         continue
-    reduce(info_arr[0], info_arr[1])
+
+    reduce(info_arr[0], info_arr[1])    
